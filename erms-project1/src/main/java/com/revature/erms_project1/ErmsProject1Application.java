a8f52c0f59d383349c99c08b872dcc2cae1efc24
zohair930 <ahmadzohair10@gmail.com>
2026-01-12 08:22:50 -0800

    Add files via upload

---
+++ erms-project1/src/main/java/com/revature/erms_project1/ErmsProject1Application.java
@@ -0,0 +1,13 @@
+package com.revature.erms_project1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErmsProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErmsProject1Application.class, args);
+	}
+
+}
