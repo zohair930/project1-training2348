5953904de7bd1efb7d4f01fde4f6055b025189fa
zohair930 <ahmadzohair10@gmail.com>
2026-01-12 07:50:45 -0800

    Add files via upload

---
+++ src/main/java/com/example/erms/ErmsApplication.java
@@ -0,0 +1,13 @@
+package com.example.erms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/erms/ErmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.erms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
