ee288902b1a5bf5a1f5081999babb16c61e44187
Zohair Ahmad <zohairahmad@Zohairs-MacBook-Pro.local>
2026-01-13 20:11:26 -0800

    Move User classes into erms_project1 package and remove duplicates

--- erms-project1/src/main/java/com/revature/erms_project1/entities/User.java
+++ erms-project1/src/main/java/com/revature/erms_project1/entities/User.java
@@ -1,4 +1,19 @@
 package com.revature.erms_project1.entities;
 
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class User {
+    private int userId;
+    private String username;
+    private String password;
+    @Enumerated(EnumType.STRING)@NonNull
+    private UserType userType;
 }
