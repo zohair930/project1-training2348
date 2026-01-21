d768672a1d3bf8a8fe98432bed78766553aaaf32
Zohair Ahmad <zohairahmad@Zohairs-MacBook-Pro.local>
2026-01-21 11:54:50 -0800

    Fixed account and ticket creation

--- erms-project1/src/main/java/com/revature/erms_project1/controllers/AccountController.java
+++ erms-project1/src/main/java/com/revature/erms_project1/controllers/AccountController.java
@@ -3,6 +3,7 @@ package com.revature.erms_project1.controllers;
 
 import com.revature.erms_project1.entities.Account;
 import com.revature.erms_project1.exceptions.AccountNotFoundException;
+import com.revature.erms_project1.exceptions.UserNotFoundException;
 import com.revature.erms_project1.services.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -19,6 +20,12 @@ public class AccountController {
     public ResponseEntity<Account> getAccount(@PathVariable Long accountId) throws AccountNotFoundException {
         Account account = accountService.getAccountById(accountId);
         return new ResponseEntity<>(account, HttpStatus.OK);
+    }
+
+    @PostMapping("/accounts")
+    public ResponseEntity<Account> createAccount(@RequestParam int userId) throws UserNotFoundException {
+        Account account = accountService.createAccountForUser(userId);
+        return new ResponseEntity<>(account, HttpStatus.CREATED);
     }
 
 }
